cff46610679d62df100b5bc1d3b10f5d78c59604
mixinspace <islammadkaur@gmail.com>
2021-04-24 21:41:26 -0500

    Added tuesdays, wednesdays, thursday, fridays, saturdays, and sundays hours for doctors.

--- src/components/user/Appointments/Doctors.js
+++ src/components/user/Appointments/Doctors.js
@@ -4,8 +4,15 @@ const Doctors = (props) => {
     const docs = props.docs.map(doc => 
         <div style={{textAlign:'center', paddingTop: '2%', paddingBottom: '2%', borderStyle: 'solid'}}> 
         <h2>Dr. {doc.first_name} {doc.last_name}</h2>
+        
         <p>Specialty: {doc.specialties.map(p => p.name)}</p>
-        <p>Mondays: {doc.mondays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Monday: {doc.mondays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Tuesday: {doc.tuesdays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Wednesday: {doc.wednesdays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Thursday: {doc.thursdays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Friday: {doc.fridays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Saturday: {doc.saturdays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
+        <p>Sunday: {doc.sundays.map(p => p.available ? `${p.start}-${p.end}` : 'N/A')} </p>
         <p>Gender: {doc.gender}</p>
         
         <img src={doc.image} alt={''} style={{height: '200px', width: '200px'}}></img>
