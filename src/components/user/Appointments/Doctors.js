f0f3b0bf64fdb333d87fe64bb29a35f6095bb8a0
mixinspace <islammadkaur@gmail.com>
2021-05-07 19:44:38 -0500

    refactored code

--- src/components/user/Appointments/Doctors.js
+++ src/components/user/Appointments/Doctors.js
@@ -1,81 +1,10 @@
-// import React from 'react';
-
-// const Doctors = (props, event) => {
-
-
-//     const docs = props.docs.map(doc => {
-        
-//           if (doc.gender === gender) {
-//     return <div style={{padding: '3%', borderStyle: 'solid'}}> 
-//         <div style={{float:'left', borderRightStyle: 'solid'}}>
-//             <img src={doc.image} alt={''} style={{height: '200px', width: '200px'}}></img>
-//             <h2>Dr. {doc.first_name} {doc.last_name}</h2>
-//             <p>Specialty: {doc.specialties.map(p => p.name)}</p>
-//             <p>Gender: {doc.gender}</p>
-//         </div>
-//         <div style={{height:'auto', marginLeft: "10%"}}>
-//             <p>Monday: {doc.mondays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Tuesday: {doc.tuesdays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Wednesday: {doc.wednesdays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Thursday: {doc.thursdays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Friday: {doc.fridays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Saturday: {doc.saturdays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//             <p>Sunday: {doc.sundays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
-//         </div>
-//     </div>
-//     }})
-
-//     const handleGender = (event) => {
-//         let gender = event.target.value
-//     } 
-
-   
-//     return (
-//         <div>
-//             <div>
-//                 <p>Gender: 
-//                 <select onChange={(event) => handleGender(event)} style={{marginLeft: '1%'}}>
-//                     <option selected={true}>-----</option>
-//                     <option value="Male">Male</option>
-//                     <option value="Female">Female</option>
-//                     <option value="All">No preference</option>
-//                 </select>
-//                 </p>
-//                 <p>Specialty:  
-//                 <select style={{marginLeft: '1%'}}>
-//                     <option selected={true}>-----</option>
-//                     <option value="Male">Male</option>
-//                     <option value="Female">Female</option>
-//                     <option value="All">No preference</option>
-//                 </select></p>
-//             </div>
-
-//             {docs}
-//         </div>
-//     );
-// };
-
-
-// export default Doctors;
-
-
 import React, { Component } from 'react';
 
 class Doctors extends Component {
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         gender: '',
-    //         specialty: ''
-    //     }
-
-    // }
 
     render(event) {
          const docs = this.props.docs.map(doc =>  { 
-            // let spec = doc.specialties.map(p => p.name)
-        
-        //  if (doc.gender == this.props.gen) {
+
                  
        return <div style={{padding: '3%', borderStyle: 'solid'}}> 
           <div style={{float:'left', borderRightStyle: 'solid'}} >
@@ -93,10 +22,8 @@ class Doctors extends Component {
               <p>Saturday: {doc.saturdays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
               <p>Sunday: {doc.sundays.map(p => p.available ? `${p.start < 12 ? `${p.start}am`: `${p.start}pm`}-${p.end < 12 ? `${p.end}am`: `${p.end - 12}pm`}` : 'N/A')} </p>
           </div>
-      </div>
-    
-    }
-    )
+      </div>   
+    })
 
         return (
             <div>
