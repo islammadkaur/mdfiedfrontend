806cfe364335b70bb932301a2457e00adcbc71b9
mixinspace <islammadkaur@gmail.com>
2021-05-07 19:45:18 -0500

    worked on filtering data

--- src/components/user/Appointments/DoctorContainer.js
+++ src/components/user/Appointments/DoctorContainer.js
@@ -29,17 +29,8 @@ class DoctorContainer extends Component {
             this.setState({ specialty: event.currentTarget.value})
         }
 
-            // console.log(this.state.gender);
-            // console.log(this.state.specialty);
-            // let spec = this.state.docs.filter(p => p.specialties.name)
-            // let specname = spec.map(p => p.name)
-
             const docs = this.state.docs
-            const spec = docs.filter(doc => doc.specialties)
-
-            // const spe = spec.filter(p => p.name == this.state.specialty)
-            // const specfilter = spec.filter(p => p.specialties.name == this.state.specialty)
-            
+            const spec = docs.filter(doc => doc.specialties)  
 
             const genfilter = this.state.docs.filter(p => p.gender === this.state.gender)
             const docfilter = this.state.docs
